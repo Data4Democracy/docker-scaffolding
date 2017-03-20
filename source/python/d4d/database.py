9eb9fe9224c38b60aa750760b9bdf65d8a0f717b
WanderingStar <github@account.loath.org>
2017-03-19 23:05:47 -0500

    support for mongo

--- source/python/d4d/database.py
+++ source/python/d4d/database.py
@@ -1,11 +1,17 @@
 import os
 import records
+from pymongo import MongoClient
 
-
-def get_db_from_env():
+def get_pg_db_from_env():
+    """Return a database connection to the Postgres database specified in docker.env"""
     db_url = 'postgresql://{user}:{passwd}@{db_host}/{db}'.format(
         user=os.environ.get('DB_USER'),
         passwd=os.environ.get('DB_PASSWORD'),
         db_host=os.environ.get('DB_HOST'),
         db=os.environ.get('DB_NAME'))
     return records.Database(db_url)
+
+
+def get_mongo_db_from_env():
+    """Return a database connection to the Mongo database specified in docker.env"""
+    return MongoClient(os.environ.get('MONGO_HOST'))
--- source/python/d4d/flask_app.py
+++ source/python/d4d/flask_app.py
@@ -1,21 +1,55 @@
-from flask import Flask
-from d4d.database import get_db_from_env
+from flask import Flask, g, render_template
+from d4d.database import get_pg_db_from_env, get_mongo_db_from_env
 
 app = Flask(__name__)
-db = get_db_from_env()
 
 
 @app.route('/')
 def hello_world():
-    rows = db.query('SELECT salutation, instances FROM greeting')
-    lines = []
+    """
+    Show all of the greetings from either DB, and increment each greeting by one
+    """
+    pg = get_pg_db()
+    rows = pg.query('SELECT salutation, instances FROM greeting')
+    greetz = []
     for r in rows:
-        lines.append("{}: {}".format(r['salutation'], r['instances']))
-        db.query('UPDATE greeting SET instances = :n WHERE salutation = :s',
-                 s=r['salutation'],
-                  n=r['instances'] + 1)
+        greetz.append(r)
+        pg.query('UPDATE greeting SET instances = instances + 1 WHERE salutation = :s', s=r['salutation'])
 
-    return 'Flask Dockerized\n' + '\n'.join(lines)
+    mongo = get_mongo_db()
+    docs = mongo.project.greeting.find({})
+    for d in docs:
+        greetz.append(d)
+        mongo.project.greeting.update({'salutation': d['salutation']},
+                                      {'$inc': {'instances': 1}})
+    app.logger.info(greetz)
+
+    return render_template('greeting.html', greetz=greetz)
+
+
+def get_pg_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'pg'):
+        g.pg = get_pg_db_from_env()
+    return g.pg
+
+def get_mongo_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'mongo'):
+        g.mongo = get_mongo_db_from_env()
+    return g.mongo
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the databases at the end of the request."""
+    if hasattr(g, 'pg'):
+        g.pg.close()
+    if hasattr(g, 'mongo'):
+        g.mongo.close()
 
 
 if __name__ == '__main__':
